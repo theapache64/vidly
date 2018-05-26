c59e0075f7513ae3e58cb0ca3bb896f62b3fe12f
theapache64 <theapache64@gmail.com>
2018-05-26 15:05:24 +0530

    Update works

--- index.js
+++ index.js
@@ -31,14 +31,38 @@ app.post('/api/genres', (req, resp) => {
     }
 
     const newGenre = {
-        id : genres.length + 1,
-        name : req.body.name
+        id: genres.length + 1,
+        name: req.body.name
     };
 
     genres.push(newGenre);
     resp.send(newGenre);
 });
 
+
+//UPDATE
+app.put('/api/genres/:id', (req, resp) => {
+
+    //Checking if the genre exist
+    const genre = genres.find(genre => genre.id === parseInt(req.params.id));
+
+    if (!genre) {
+        return resp.status(404).send(`Invalid genre ID ${req.params.id}`);
+    }
+
+    const {error} = validateGenre(req.body);
+    if (error) {
+        return resp.status(400).send(error.details[0].message);
+    }
+
+    //Updates
+    genre.name = req.body.name;
+    resp.send(genre);
+});
+
+//DELETE
+
+//Base method to validate genre
 function validateGenre(genre) {
     const schema = {
         name: Joi.string().min(3).required()
@@ -46,10 +70,6 @@ function validateGenre(genre) {
 
     return Joi.validate(genre, schema);
 }
-
-//UPDATE
-
-//DELETE
 
 const port = process.env.PORT || 7777;
 
