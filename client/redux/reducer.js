1e2aaa796cda9127c98275bdf59390ae6095f763
theapache64 <theapache64@gmail.com>
2018-05-29 09:18:41 +0530

    Concurrent environment setup for express and react

---
+++ client/redux/reducer.js
@@ -0,0 +1,8 @@
+// @flow
+export const GET_GENRES = 'GET_GENRES';
+export const GET_GENRES_SUCCESS = 'GET_GENRES_SUCCESS';
+export const GET_GENRES_FAIL = 'GET_GENRES_FAIL';
+
+export default genreReducer = (state = {}, action) => {
+
+}
--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,35 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    state = {
+        response: ''
+    };
+
+    componentDidMount() {
+        this.callApi()
+            .then(res => this.setState({ response: res.genres }))
+            .catch(err => console.log(err));
+    }
+
+    callApi = async () => {
+        const response = await fetch('/api/genres');
+        const body = await response.json();
+
+        if (response.status !== 200) throw Error(body.genres);
+
+        return body;
+    };
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
 }
 
 export default App;
